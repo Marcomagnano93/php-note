1ce486b59e0727c6c418a2cd320ae5a6d64c698d
Marcomagnano93 <marcog.magnano@gmail.com>
2024-09-19 17:11:01 +0200

    ADD: bootstrap

--- views/index.view.php
+++ views/index.view.php
@@ -6,10 +6,15 @@
         <?php require 'partials/header.php'; ?>
          <!-- main -->
          <main>
-
+            <div class="container">
+                <h1>Questa è la home</h1>
+            </div>
+            
          </main>
          <!-- footer -->
         <?php require 'partials/footer.php'; ?>
     </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
